a5a3ac88f83a41141abeff778b455e669345de1c
danparry <dan.parry22@gmail.com>
2020-08-25 15:39:39 +0200

    elements and animation

--- paclogic.js
+++ paclogic.js
@@ -6,52 +6,52 @@ document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid');
   const layout = [
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    1,3,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,3,0,0,0,1,
-    1,0,1,1,1,0,1,0,1,0,1,1,1,0,1,0,1,1,1,1,1,1,0,1,1,0,1,0,1,
-    1,0,1,1,1,0,1,0,1,0,1,0,0,0,0,0,0,0,1,1,0,0,0,1,1,0,1,0,1,
-    1,0,0,0,0,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,0,1,0,1,1,0,1,0,1,
+    1,7,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,3,0,0,0,1,
+    1,0,1,1,1,0,1,0,1,0,1,1,1,8,1,0,1,1,0,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,4,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,1,0,0,0,1,4,0,1,0,1,
+    1,0,1,0,0,0,0,0,1,0,1,0,1,0,1,0,1,0,1,0,0,1,0,1,1,0,1,0,1,
     1,0,1,1,1,1,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,0,0,0,0,1,0,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,1,0,1,
     1,0,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,0,1,1,1,0,1,0,0,0,0,0,1,
     1,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,1,1,0,1,
-    1,0,1,1,1,0,1,0,1,0,1,1,1,1,1,1,1,1,1,0,1,0,1,0,0,0,0,0,1,
+    1,0,1,1,1,0,1,0,1,0,1,1,1,4,1,4,1,1,1,0,1,0,1,0,0,0,0,0,1,
     1,0,1,1,1,0,1,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,1,0,1,1,1,0,1,
     1,0,0,0,0,0,0,0,1,0,1,1,1,4,4,4,1,1,1,0,1,0,0,0,0,0,0,0,1,
     1,1,1,1,1,1,1,0,4,0,1,2,2,2,2,2,2,2,1,0,4,0,1,1,1,1,1,1,1,
-    5,4,4,1,1,4,4,0,1,0,1,2,2,2,2,2,2,2,1,0,1,0,1,4,4,4,4,1,1,
-    1,1,4,4,4,4,1,0,4,0,1,2,2,2,2,2,2,2,1,0,4,0,4,4,1,1,4,4,6,
-    1,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,
-    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    5,4,4,4,4,4,4,0,1,0,2,2,2,2,2,2,2,2,2,0,1,0,4,4,4,4,4,4,6,
+    5,4,4,4,4,4,4,0,4,0,1,2,2,2,2,2,2,2,1,0,4,0,4,4,4,4,4,4,6,
+    1,1,1,1,1,1,1,0,1,0,1,1,1,1,4,1,1,1,1,0,1,0,1,1,1,1,1,1,1,
+    1,0,0,0,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
     1,0,1,1,1,0,1,0,1,0,1,1,1,1,1,1,1,1,1,0,1,0,0,0,1,1,1,0,1,
     1,0,1,1,1,0,1,0,1,0,1,0,0,0,4,0,0,0,1,0,1,0,1,0,0,1,1,0,1,
     1,0,0,0,0,0,1,0,1,0,1,0,1,1,1,1,1,0,1,0,1,0,0,1,0,0,1,0,1,
     1,0,1,1,1,1,1,0,1,0,1,0,1,0,0,0,1,0,1,0,1,1,0,0,1,0,0,0,1,
     1,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,1,0,0,1,1,0,1,
     1,0,1,1,1,1,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,0,1,0,0,0,0,1,
-    1,3,0,0,0,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,1,0,0,1,3,1,0,1,
+    1,3,0,0,0,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,1,0,0,1,7,1,0,1,
     1,0,1,1,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,1,1,0,0,0,0,0,1,
     1,0,1,1,1,0,1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1,1,1,1,1,0,1,
     1,0,0,0,0,0,0,0,1,0,0,0,0,1,1,1,0,0,0,0,1,0,0,0,0,0,0,0,1,
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
-
-  ]
-  // 0 - pac-dots
+ ];
+  // 0 - pac-dots       
   // 1 - wall
-  // 2 - ghost-lair
-  // 3 - power-pellet
+  // 2 - ghost-lair     
+  // 3 - toiletpaper (powerUp)
   // 4 - empty
-  // 5 - teleport
+  // 5 - teleporterLeft  
+  // 6 -teleporterRight
+  // 7 - hand sanitizer (powerUp)
+  // 8 - mask (powerUp)
 
   const squares = []
 
-  //create your board
   function createBoard() {
     for (let i = 0; i < layout.length; i++) {
       const square = document.createElement('div')
       grid.appendChild(square)
       squares.push(square)
 
-      //add layout to the board
       if(layout[i] === 0) {
         squares[i].classList.add('pac-dot')
       } else if (layout[i] === 1) {
@@ -59,25 +59,27 @@ document.addEventListener('DOMContentLoaded', () => {
       } else if (layout[i] === 2) {
         squares[i].classList.add('ghost-lair')
       } else if (layout[i] === 3) {
-        squares[i].classList.add('power-pellet')
+        squares[i].classList.add('power-pellet-paper')
       }
       else if (layout[i] === 5) {
         squares[i].classList.add('teleportLeft')
-      }
-      else if (layout[i] === 6) {
+      } else if (layout[i] === 6) {
         squares[i].classList.add('teleportRight')
-      }
+      } else if (layout[i] === 7) {
+      squares[i].classList.add('power-pellet-hand')
+    }else if (layout[i] === 8) {
+      squares[i].classList.add('power-pellet-mask')
+    }
     }
   }
+
+  
   createBoard()
 
 
-let pacmanCurrentIndex = 536
+let pacmanCurrentIndex = 536  //536
 
 squares[pacmanCurrentIndex].classList.add('pac-man')
-
-
-
 
 function getCoordinates(index) {
 return [index % width, Math.floor(index / width)]
@@ -98,6 +100,7 @@ switch(e.keyCode) {
     pacmanCurrentIndex -=1
 
     if((pacmanCurrentIndex - 1) === 377) {pacmanCurrentIndex = 434}
+    if((pacmanCurrentIndex - 1) === 406) {pacmanCurrentIndex = 405}
     break
   case 38:
     if(pacmanCurrentIndex - width >= 0 && !squares[pacmanCurrentIndex -width].classList.contains('wall')
@@ -110,6 +113,7 @@ switch(e.keyCode) {
     pacmanCurrentIndex +=1
 
     if((pacmanCurrentIndex +1)  === 434) {pacmanCurrentIndex = 377}
+    if((pacmanCurrentIndex +1)  === 405) {pacmanCurrentIndex = 406}
     break
   case 40:
     if(pacmanCurrentIndex + width < width * width && !squares[pacmanCurrentIndex +width].classList.contains('wall')
@@ -119,14 +123,11 @@ switch(e.keyCode) {
 }
 
 squares[pacmanCurrentIndex].classList.add('pac-man')
-// let pacman = document.querySelector(".pac-man")
-// const mouth = document.createElement('div')
-// mouth.classList.add('pacman-mouth')
-// pacman.appendChild(mouth)
 
 
 pacDotEaten()
-powerPelletEaten()
+powerPelletPaper()
+powerPalletHand()
 checkForGameOver()
 checkForWin()
 
@@ -137,17 +138,35 @@ function pacDotEaten(){
 
   if(squares[pacmanCurrentIndex].classList.contains('pac-dot')) {
     score++
-    scoreDisplay.innerHTML=score
+    scoreDisplay.innerHTML=score;
     squares[pacmanCurrentIndex].classList.remove('pac-dot')
   }
 }
 
-function powerPelletEaten() {
-  if (squares[pacmanCurrentIndex].classList.contains('power-pellet')) {
+function powerPelletPaper() {
+  if (squares[pacmanCurrentIndex].classList.contains('power-pellet-paper')) {
     score +=10
     ghosts.forEach(ghost => ghost.isScared = true)
     setTimeout(unScareGhosts, 10000);
-    squares[pacmanCurrentIndex].classList.remove('power-pellet')
+    squares[pacmanCurrentIndex].classList.remove('power-pellet-paper')
+  }
+}
+
+function powerPalletHand() {
+  if (squares[pacmanCurrentIndex].classList.contains('power-pellet-hand')) {
+    score +=10
+    ghosts.forEach(ghost => ghost.isScared = true)
+    setTimeout(unScareGhosts, 10000);
+    squares[pacmanCurrentIndex].classList.remove('power-pellet-hand')
+  }
+}
+
+function powerPalletMask() {
+  if (squares[pacmanCurrentIndex].classList.contains('power-pellet-mask')) {
+    score +=10
+    ghosts.forEach(ghost => ghost.isScared = true)
+    setTimeout(unScareGhosts, 10000);
+    squares[pacmanCurrentIndex].classList.remove('power-pellet-mask')
   }
 }
 
@@ -170,9 +189,9 @@ class Ghost {
 ghosts = [
   new Ghost('blinky', 388, 250),
   new Ghost('pinky', 392, 270),
-  new Ghost('inky', 417, 300),
+  new Ghost('inky', 111, 200),
   new Ghost('clyde', 422, 400),
-  new Ghost('bob', 391, 150 )
+  new Ghost('bob', 90, 150 ) // 391
 ];
 
 ghosts.forEach(ghost => {
@@ -216,15 +235,15 @@ checkForGameOver()
     if (squares[pacmanCurrentIndex].classList.contains('ghost') &&
       !squares[pacmanCurrentIndex].classList.contains('scared-ghosts')) {
       ghosts.forEach(ghost => clearInterval(ghost.timerId))
-      document.removeEventListener('keyup', movePacman)
+      document.removeEventListener('keydown', movePacman)
       setTimeout(function(){ alert("Game Over"); }, 500)
     }
   }
 
   function checkForWin() {
-    if (score > 450) {
+    if (score > 400) {
       ghosts.forEach(ghost => clearInterval(ghost.timerId))
-      document.removeEventListener('keyup', movePacman)
+      document.removeEventListener('keydown', movePacman)
       setTimeout(function(){ alert("WINNER WINNER!"); }, 500)
     }
   }
