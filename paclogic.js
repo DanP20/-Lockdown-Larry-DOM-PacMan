df4af5bf10ae8766afcedf587a969631889f57d0
danparry <dan.parry22@gmail.com>
2020-08-26 17:37:17 +0200

    audio

--- paclogic.js
+++ paclogic.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const scoreDisplay = document.getElementById('score');
+  const gameOver = document.getElementById("gameOver");
+  const powerUp = document.getElementById("powerUp");
+  const move = document.getElementById("move");
+  const gameMusic = document.getElementById("gameMusic");
   const width = 36;
   let score = 0;
   const grid = document.querySelector('.grid');
@@ -8,16 +12,16 @@ document.addEventListener('DOMContentLoaded', () => {
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
     1,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,1,
     1,0,1,1,1,1,1,0,1,0,8,1,0,0,1,0,0,1,1,0,0,1,0,0,1,0,0,1,0,1,0,1,1,1,0,1,
-    1,0,0,0,2,2,1,0,0,1,0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,1,11,1,0,1,
+    1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,1,11,1,0,1,
     1,0,1,0,1,1,1,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,0,1,0,1,0,1,0,1,
     1,0,1,0,0,0,0,0,11,0,0,1,0,0,1,0,0,0,0,0,0,1,3,0,1,0,0,0,0,0,0,0,0,1,0,1,
     1,0,1,0,1,1,1,0,1,1,0,0,1,0,0,0,1,1,1,1,0,0,0,1,0,0,1,1,0,1,1,1,0,1,0,1,
     1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,2,2,2,2,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,
     1,1,1,1,0,0,0,1,1,1,0,0,1,1,1,2,2,2,2,2,2,1,1,1,0,0,1,1,1,0,0,0,1,1,1,1,
-    9,4,4,4,4,1,0,4,0,0,0,0,1,2,2,2,2,2,2,2,2,2,2,1,0,0,0,0,0,0,1,0,4,4,4,6,
-    5,4,4,4,0,1,0,0,0,0,0,0,2,2,2,2,2,2,2,2,2,2,2,2,11,2,0,0,0,0,1,0,4,4,4,10,
-    1,1,1,1,0,0,0,1,1,1,3,4,2,2,2,2,2,2,4,4,4,4,2,2,0,0,1,1,1,0,0,0,1,1,1,1,
-    1,0,0,0,0,0,0,0,0,1,0,0,1,2,2,2,2,2,2,2,2,2,2,1,0,0,1,0,0,0,0,0,0,0,0,1,
+    9,4,4,4,4,1,0,4,0,0,0,0,1,2,2,2,2,2,2,2,2,2,2,1,0,0,0,0,0,0,1,11,4,4,4,6,
+    5,4,4,4,0,1,0,0,0,0,0,0,2,2,2,2,2,2,2,2,2,2,2,2,0,0,0,0,0,0,1,0,4,4,4,10,
+    1,1,1,1,0,0,0,1,1,1,0,0,2,2,2,2,2,2,4,4,4,4,2,2,0,0,1,1,1,0,0,0,1,1,1,1,
+    1,0,0,0,0,0,0,0,3,1,0,0,1,2,2,2,2,2,2,2,2,2,2,1,0,0,1,0,0,0,0,0,0,0,0,1,
     1,4,1,1,1,1,11,0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,1,1,1,1,0,1,
     1,4,1,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,
     1,0,1,0,1,0,1,0,0,0,1,1,1,1,0,1,1,1,1,1,1,0,1,1,1,1,0,0,0,1,0,1,0,1,0,1,
@@ -37,13 +41,18 @@ document.addEventListener('DOMContentLoaded', () => {
   // 8 - mask (powerUp)
   // 9 - teleporterLeftTop 
   // 10 - teleportRightBottom
+  // 11 - Covid
+  //12 - Vaccine
 
 
   //BUILD BOARD
 
   const squares = []
+ 
 
   function createBoard() {
+
+   
     for (let i = 0; i < layout.length; i++) {
       const square = document.createElement('div')
       grid.appendChild(square)
@@ -71,8 +80,11 @@ document.addEventListener('DOMContentLoaded', () => {
       squares[i].classList.add('teleportRightBottom')
       } else if (layout[i] === 11) {
       squares[i].classList.add('power-pellet-covid')
+     } else if (layout[i] === 12) {
+      squares[i].classList.add('power-pellet-vaccine')
+     }
     }
-    }
+    
   }
 
   
@@ -80,7 +92,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // PACMAN
 
-let pacmanCurrentIndex = 522  //536
+let pacmanCurrentIndex = 522  //522  395 rightbot  360 botleft  359righttop  314
+
+
 
 squares[pacmanCurrentIndex].classList.add('pac-man')
 
@@ -89,22 +103,27 @@ return [index % width, Math.floor(index / width)]
   }
 console.log(getCoordinates(pacmanCurrentIndex))
 
+
+
 // MOVEMENT
 
 function movePacman(e) {
 
+  const gameTheme = true;
+
+
 squares[pacmanCurrentIndex].classList.remove('pac-man')
 
 
-
 switch(e.keyCode) {
+  
   case 37:
     if(pacmanCurrentIndex % width !== 0 && !squares[pacmanCurrentIndex -1].classList.contains('wall')
     && !squares[pacmanCurrentIndex -1].classList.contains('ghost-lair'))
     pacmanCurrentIndex -=1
 
-    if((pacmanCurrentIndex - 1) === 507) {pacmanCurrentIndex = 544}
-    if((pacmanCurrentIndex - 1) === 544) {pacmanCurrentIndex = 507}
+    if((pacmanCurrentIndex - 1) === 324) {pacmanCurrentIndex = 359}
+    if((pacmanCurrentIndex - 1) === 360) {pacmanCurrentIndex = 395}
     break
   case 38:
     if(pacmanCurrentIndex - width >= 0 && !squares[pacmanCurrentIndex -width].classList.contains('wall')
@@ -113,18 +132,22 @@ switch(e.keyCode) {
     break
   case 39:
     if(pacmanCurrentIndex % width < width -1 && !squares[pacmanCurrentIndex +1].classList.contains('wall')
-    && !squares[pacmanCurrentIndex % width].classList.contains('ghost-lair'))
+    && !squares[pacmanCurrentIndex +1].classList.contains('ghost-lair'))
     pacmanCurrentIndex +=1
 
-    if((pacmanCurrentIndex +1)  === 434) {pacmanCurrentIndex = 377}
-    if((pacmanCurrentIndex +1)  === 405) {pacmanCurrentIndex = 406}
+    if((pacmanCurrentIndex +1)  === 359) {pacmanCurrentIndex = 324}
+    if((pacmanCurrentIndex +1)  === 395) {pacmanCurrentIndex = 360}
     break
   case 40:
     if(pacmanCurrentIndex + width < width * width && !squares[pacmanCurrentIndex +width].classList.contains('wall')
     && !squares[pacmanCurrentIndex + width].classList.contains('ghost-lair'))
     pacmanCurrentIndex += width
     break
+
 }
+
+
+
 
 squares[pacmanCurrentIndex].classList.add('pac-man')
 
@@ -134,7 +157,15 @@ powerPelletPaper()
 powerPelletHand()
 powerPelletMask()
 powerPelletCovid()
-
+pacDotEaten()
+checkForGameOver()
+checkForWin()
+if (gameTheme) {
+  gameMusic.pause();
+  gameMusic.currentPlayTime = 0;
+  gameMusic.play();
+ gameMusic = false;
+  }
 
 }
 document.addEventListener('keydown', movePacman)
@@ -143,48 +174,82 @@ document.addEventListener('keydown', movePacman)
 //POWERUPS AND DEBUFFS
 
 function pacDotEaten(){
+  const pacmanMove = true;
 
   if(squares[pacmanCurrentIndex].classList.contains('pac-dot')) {
     score++
     scoreDisplay.innerHTML=score;
     squares[pacmanCurrentIndex].classList.remove('pac-dot')
+    if (pacmanMove) {
+      move.pause();
+      move.currentPlayTime= 0;
+      move.play();
+      pacmanMove = false;
+    }
   }
+  
+  
 }
-pacDotEaten()
 
 
-function powerPelletPaper() {
+
+function powerPelletPaper() { 
+  const powerUpEaten = true;
   if (squares[pacmanCurrentIndex].classList.contains('power-pellet-paper')) {
     score +=50
+    scoreDisplay.innerHTML=score;
     ghosts.forEach(ghost => ghost.isScared = true)
     setTimeout(unScareGhosts, 10000);
-    squares[pacmanCurrentIndex].classList.remove('power-pellet-paper')
+    squares[pacmanCurrentIndex].classList.remove('power-pellet-paper') 
+    if (powerUpEaten) {
+      powerUp.pause();
+      powerUp.currentTime = 0;
+      powerUp.play();
+      powerUpEaten = false;
+    }
   }
 }
 
 function powerPelletHand() {
+  const powerUpEaten = true;
   if (squares[pacmanCurrentIndex].classList.contains('power-pellet-hand')) {
     score += 50
+    scoreDisplay.innerHTML=score;
     ghosts.forEach(ghost => ghost.isScared = true)
     setTimeout(unScareGhosts, 10000);
     squares[pacmanCurrentIndex].classList.remove('power-pellet-hand')
+    if (powerUpEaten) {
+      powerUp.pause();
+      powerUp.currentTime = 0;
+      powerUp.play();
+      powerUpEaten = false;
+    }
   }
 }
 
 function powerPelletMask() {
+  const powerUpEaten = true;
   if (squares[pacmanCurrentIndex].classList.contains('power-pellet-mask')) {
     score += 50
+    scoreDisplay.innerHTML=score;
     ghosts.forEach(ghost => ghost.isScared = true)
     setTimeout(unScareGhosts, 10000);
     squares[pacmanCurrentIndex].classList.remove('power-pellet-mask')
+    if (powerUpEaten) {
+      powerUp.pause();
+      powerUp.currentTime = 0;
+      powerUp.play();
+      powerUpEaten = false;
+    }
   }
 }
 
 function powerPelletCovid() {
   if (squares[pacmanCurrentIndex].classList.contains('power-pellet-covid')) {
     score -=100
-    ghosts.forEach(ghost => ghost.isFast = true)
-    setTimeout(isNotFast, 10000);
+    scoreDisplay.innerHTML=score;
+    // ghosts.forEach(ghost => ghost.isFast = true)
+    // setTimeout(isNotFast, 10000);
     squares[pacmanCurrentIndex].classList.remove('power-pellet-covid')
   }
 }
@@ -193,13 +258,13 @@ function unScareGhosts() {
   ghosts.forEach(ghost => ghost.isScared = false)
 }
 
-function isNotFast() {
-  ghosts.forEach(ghost => ghost.isFast = false)
-}
+// function isNotFast() {
+//   ghosts.forEach(ghost => ghost.isFast = false)
+// }
 
-function isFast() {
-  ghosts.forEach(ghost.speed += 100)
-}
+// function isFast() {
+//   ghosts.forEach(ghost.speed += 100)
+// }
 
 // GHOSTS
 
@@ -211,16 +276,16 @@ class Ghost {
   this.currentIndex = startIndex;
   this.timerId = NaN;
   this.isScared = false;
-  this.isFast = false;
+  // this.isFast = false;
   }
 }
 
 ghosts = [
-  new Ghost('blinky', 410, 250),
-  new Ghost('pinky', 416, 270),
-  new Ghost('inky', 380, 200),
-  new Ghost('clyde', 375, 400),
-  new Ghost('bob', 90, 250 ) // 391
+  new Ghost('blinky', 411, 330),
+  new Ghost('pinky', 416, 370),
+  new Ghost('inky', 380, 300),
+  new Ghost('clyde', 375, 250),
+  new Ghost('bob', 450, 370 ) // 391
 ];
 
 ghosts.forEach(ghost => {
@@ -231,8 +296,11 @@ ghosts.forEach(ghost => {
 ghosts.forEach(ghost => moveGhost(ghost))
 
   function moveGhost(ghost) {
+
+    const ghostsSiren = true;
     const directions =  [-1, +1, width, -width]
     let direction = directions[Math.floor(Math.random() * directions.length)]
+   
 
   
  ghost.timerId = setInterval(function() {
@@ -255,28 +323,41 @@ ghosts.forEach(ghost => moveGhost(ghost))
         squares[ghost.currentIndex].classList.add(ghost.className, 'ghost')
       }
 
+      //  if (ghostsSiren) {
+      //  ghostsMove.pause();
+      //   ghostsMove.play();
+      //    ghostsSiren = false;
+      //  }
+
 checkForGameOver()
 
  }, ghost.speed)
   }
 
   // WIN/LOSE LOGIC
-  function checkForGameOver() {
+  function checkForGameOver()
+  { const pacmanDead = true;
     if (squares[pacmanCurrentIndex].classList.contains('ghost') &&
       !squares[pacmanCurrentIndex].classList.contains('scared-ghosts')) {
       ghosts.forEach(ghost => clearInterval(ghost.timerId))
       document.removeEventListener('keydown', movePacman)
       setTimeout(function(){ alert("Game Over"); }, 500)
+      if (pacmanDead) {
+        gameOver.pause();
+        gameOver.currentTime = 0;
+        gameOver.play();
+        pacmanDead = false;
+      }
     }
   }
 
   function checkForWin() {
-    if (score > 550) {
+    if (score > 900) {
       ghosts.forEach(ghost => clearInterval(ghost.timerId))
       document.removeEventListener('keydown', movePacman)
       setTimeout(function(){ alert("WINNER WINNER!"); }, 500)
     }
   }
-  checkForGameOver()
-checkForWin()
+ 
+ 
 });
